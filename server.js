0bfe59000915585335d6c578eff8ba292dbe2b1f
suryaSekar6 <122504480+suryaSekar6@users.noreply.github.com>
2026-07-21 15:25:57 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
+      "http://localhost:5173",
       "https://suryasekar.netlify.app" // ❌ NO trailing slash
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
